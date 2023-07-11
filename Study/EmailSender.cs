9c6d81128b71e5964d28a99d0cbc79639b7f09c9
Victor <victor.motta1502@gmail.com>
2023-07-10 22:20:51 -0300

    Classes FileReader e EmailSender cosntruídas

--- Study/EmailSender.cs
+++ Study/EmailSender.cs
@@ -4,19 +4,69 @@ using System.Linq;
 using System.Text;
 using MailKit;
 using MimeKit;
-using MailKit.Net.Smtp;
 using System.Threading.Tasks;
+using System.Net.Mail;
+using System.Globalization;
+using System.Net;
 
 namespace Notificador
 {
     internal class EmailSender
     {
-        private string mail;
-        private string smtp_server;
-        private string subject;
-        
-        public EmailSender(string mail) {
-            this.mail = mail;
+        private string email;
+        private string smtpClientData;
+        private string port;
+        private string password;
+        private FileReader fileData = new();
+        private List<string> customerEmail = new();
+        private MailMessage mailMessage = new();
+
+        public EmailSender(string asset, bool action) {
+            
+            email = fileData.Email;
+            smtpClientData = fileData.SmtpClient;
+            password = fileData.Password;
+            port = fileData.Port;
+            customerEmail = fileData.EmailList;
+            
+            try
+            {
+                mailMessage.From = new MailAddress(email);
+                if (action)
+                {
+                    mailMessage.Subject = "Sua ação " + asset + " atingiu o ponto de VENDA! StockAlert";
+                    mailMessage.Body = "<html><body> Hora de vender a sua ação<body><html>";
+                    mailMessage.IsBodyHtml = true;
+                    for(int i = 0; i < customerEmail.Count; i++)
+                    {
+                        mailMessage.To.Add(new MailAddress(customerEmail[i]));
+                    }
+                }
+                else 
+                {
+                    mailMessage.Subject = "Sua ação " + asset + " atingiu o ponto de COMPRA! StockAlert";
+                    mailMessage.Body = "<html><body> Hora de comprar a sua ação<body><html>";
+                    mailMessage.IsBodyHtml = true;
+                    for (int i = 0; i < customerEmail.Count; i++)
+                    {
+                        mailMessage.To.Add(new MailAddress(customerEmail[i]));
+                    }
+                }
+                var smtpClient = new SmtpClient(smtpClientData)
+                {
+                    Port = int.Parse(this.port),
+                    Credentials = new NetworkCredential(email, password),
+                    EnableSsl = true,
+                };
+                smtpClient.Send(mailMessage);
+                
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+
         }
     }
 }
--- Study/FileReader.cs
+++ Study/FileReader.cs
@@ -5,18 +5,42 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 using System.Runtime.CompilerServices;
+using System.Diagnostics;
 
 namespace Notificador
 {
-    internal static class FileReader
+    internal class FileReader
     {
-        private static string smtp_server;
-        private static string smtp_credentials;
-        public static void readTxt()
+        private string? smtp_client;
+        private string? port;
+        private string? email;
+        private string? password;
+        private string? file_string;
+        private List<string> recipient_list = new List<string>();
+
+        public FileReader()
         {
             try
             {
-                StreamReader reader = new StreamReader("smtp\\SMTP_Config_file.txt");
+                if (File.Exists("smtp\\SMTP_Config_file.txt"))
+                {
+                    file_string = File.ReadAllText("smtp\\SMTP_Config_file.txt");
+                    string[] substring_array = file_string.Split(' ');
+                    smtp_client = substring_array[0];
+                    port = substring_array[1];
+                    email = substring_array[2];
+                    password = substring_array[3];
+
+
+                    for (int i = 4; i < substring_array.Length; i++)
+                    {
+                        recipient_list.Add(substring_array[i]);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Não foi possível encontrar o arquivo no caminho especificado: smtp\\SMTP_Config_file.txt");
+                }
             }
             catch(Exception ex)
             {
@@ -24,7 +48,11 @@ namespace Notificador
             }
 
         }
-
+        public string SmtpClient => smtp_client;
+        public string Port => port;
+        public string Email => email;
+        public string Password => password;
+        public List<string> EmailList=> recipient_list;
 
     }
 }
