60caabaae4ca6d3e555ad35cdac6549e8a55ff05
Victor <victor.motta1502@gmail.com>
2023-07-16 22:32:45 -0300

    Substituição do goto por uma abordagem mais coesa

--- Study/Program.cs
+++ Study/Program.cs
@@ -7,7 +7,8 @@ using Newtonsoft.Json;
 using System.Collections.Generic;
 using Newtonsoft.Json.Linq;
 
-namespace Notificador{
+namespace Notificador
+{
     class Program
     {
         public static async Task Main(string[] args)
@@ -19,61 +20,70 @@ namespace Notificador{
             double last_value = 0;
             string? quoteDataJson;
 
-            Start:
-            do
+            while (true)
             {
-                try
+                do
                 {
-                    input = Console.ReadLine();
-                    string[] value = input.Split(' ');
-                    asset = value[0];
-                    min_value = double.Parse(value[1]);
-                    max_value = double.Parse(value[2]);
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex.Message);
-                }
-            }while (string.IsNullOrEmpty(input) || double.IsNegative(min_value) || double.IsNegative(max_value));
+                    try
+                    {
+                        input = Console.ReadLine();
+                        string[] value = input.Split(' ');
+                        asset = value[0];
+                        min_value = double.Parse(value[1]);
+                        max_value = double.Parse(value[2]);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                } while (string.IsNullOrEmpty(input) || double.IsNegative(min_value) || double.IsNegative(max_value));
 
-            while(true)
-            {
-                try
+                while (true)
                 {
-                    ApiConnect apiConnect = new ApiConnect();
-                    quoteDataJson = await apiConnect.GetQuoteData(asset);
-                    if (quoteDataJson == null)
+                    try
                     {
-                        goto Start;
-                    }
-                    JObject quoteData = JObject.Parse(quoteDataJson);
-                    double regularMarketPrice = (double)quoteData["results"][0]["regularMarketPrice"];
+                        ApiConnect apiConnect = new ApiConnect();
+                        quoteDataJson = await apiConnect.GetQuoteData(asset);
+                        if (quoteDataJson == null)
+                        {
+                            break;
+                        }
+                        JObject quoteData = JObject.Parse(quoteDataJson);
+                        double regularMarketPrice = (double)quoteData["results"][0]["regularMarketPrice"];
 
-                    if (regularMarketPrice < min_value && (last_value > min_value || last_value == 0))
-                    {
-                        Console.WriteLine($"O valor do ativo {asset} é inferior ao valor de compra determinado. Enviando E-Mail ao cliente");
-                        EmailSender sendMail = new EmailSender(asset, false);
-                        sendMail.SendMailSmtp();
+                        if (regularMarketPrice < min_value && (last_value > min_value || last_value == 0))
+                        {
+                            Console.WriteLine($"O valor do ativo {asset} é inferior ao valor de compra determinado.({regularMarketPrice}) Enviando E-Mail ao cliente");
+                            EmailSender sendMail = new EmailSender(asset, false);
+                            sendMail.SendMailSmtp();
+                        }
+                        else if (regularMarketPrice > max_value && last_value < max_value)
+                        {
+                            Console.WriteLine($"O valor do ativo {asset} é superior ao valor de venda determinado.({regularMarketPrice}) Enviando E-Mail ao cliente");
+                            EmailSender sendMail = new EmailSender(asset, true);
+                            sendMail.SendMailSmtp();
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Valor atual do ativo {asset}: {regularMarketPrice}");
+                        }
+                        last_value = regularMarketPrice;
                     }
-                    else if (regularMarketPrice > max_value && last_value < max_value)
+                    catch (Exception ex)
                     {
-                        Console.WriteLine($"O valor do ativo {asset} é superior ao valor de venda determinado. Enviando E-Mail ao cliente");
-                        EmailSender sendMail = new EmailSender(asset, true);
-                        sendMail.SendMailSmtp();
+                        Console.WriteLine($"Não foi possível conectar-se à API, ocorreu um erro: {ex.Message}");
                     }
-                    else
-                    {
-                        Console.WriteLine($"Valor atual do ativo {asset}: {regularMarketPrice}");
-                    }
-                    last_value = regularMarketPrice;
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine($"Não foi possível conectar-se à API, ocorreu um erro: {ex.Message}");
-                }
 
-            Thread.Sleep(TimeSpan.FromMinutes(5));
-            }   
+                    await Task.Delay(TimeSpan.FromMinutes(5));
+                }
+            }
         }
     }
 }
+
+
+
+
+
+
+
